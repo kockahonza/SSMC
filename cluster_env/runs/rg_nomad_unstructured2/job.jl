57d60869bf1a41eaa0d6cdfed33253818dc3a769
Jan Kocka <kockahonza@gmail.com>
2025-08-21 10:43:29 +0100

    progress

--- cluster_env/runs/rg_nomad_unstructured2/job.jl
+++ cluster_env/runs/rg_nomad_unstructured2/job.jl
@@ -76,7 +76,7 @@ function main1(;
     num_starts=5,
     num_prescreens=50,
     num_repeats=1000,
-    max_nomad_time=60 * 60 * 2,
+    max_nomad_time=60 * 60 * 1,
     max_single_solver_time=5,
     granularity=1e-3,
 )
