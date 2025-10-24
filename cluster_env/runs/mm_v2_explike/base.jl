1cfbd9168890951cfd451949b233eea2f8aea86d
Jan Kocka <kockahonza@gmail.com>
2025-10-24 14:49:09 +0100

    progress

--- cluster_env/runs/mm_v2_explike/base.jl
+++ cluster_env/runs/mm_v2_explike/base.jl
@@ -155,68 +155,6 @@ function main2()
                 make_cartesianspace_smart(1; dx),
                 # nthreads()
             )
-            sp = make_smmicrm_problem(sps, copy(u0), 1e6)
-
-            tol = 100 * eps()
-            s = solve(sp, QNDF();
-                abstol=tol,
-                reltol=tol,
-                callback=make_timer_callback(10 * 60)
-            )
-
-            params[i, j] = sps
-            retcodes[i, j] = s.retcode
-            final_states[i, j] = s.u[end]
-
-            GC.gc()
-
-            next!(prog)
-            flush(stdout)
-        end
-        # @printf "Finished %d out of %d logK runs\n" i length(logKs)
-    end
-    finish!(prog)
-
-    jldsave("main2_results.jld2"; logKs, ls, params, retcodes, final_states, L, sN, epsilon)
-
-    (; params, retcodes, final_states)
-end
-
-function main2light()
-    logKs = range(-0.5, 3, 100)
-    ls = range(0.0, 1.0, 50)
-    # Spatial setup
-    L = 2 # system size in non-dim units
-    sN = 10000 # number of spatial points
-    epsilon = 1e-5 # initial condition noise amplitude
-
-    dx = L / (sN + 1)
-    u0 = clamp.(reduce(hcat, [[1.0, 0.0, 0.0] .+ epsilon .* randn(3) for _ in 1:sN]), 0.0, Inf)
-
-    params = Matrix{Any}(undef, length(logKs), length(ls))
-    retcodes = Matrix{ReturnCode.T}(undef, length(logKs), length(ls))
-    final_states = Matrix{Matrix{Float64}}(undef, length(logKs), length(ls))
-    prog = Progress(length(logKs) * length(ls))
-
-    GC.enable_logging(true)
-    @tasks for i in 1:length(logKs)
-        logK = logKs[i]
-        for (j, l) in enumerate(ls)
-            l = ls[j]
-            mmp = MMParams(;
-                K=10^logK,
-                m=1.0,
-                l=l,
-                k=0.0,
-                c=1.0,
-                d=1.0,
-            )
-            sps = SASMMiCRMParams(
-                mmp_to_mmicrm(mmp),
-                SA[1e-12, 1.0, 1.0],
-                make_cartesianspace_smart(1; dx),
-                # nthreads()
-            )
             sp = make_smmicrm_problem(sps, copy(u0), 1e8)
 
             tol = 100 * eps()
@@ -289,6 +227,70 @@ function main3()
     finish!(prog)
 
     jldsave("main3_results.jld2"; logKs, ls, params, retcodes, final_states)
+
+    (; params, retcodes, final_states)
+end
+
+function main4()
+    logKs = range(-0.1, 1, 100)
+    ls = range(0.0, 1.0, 20)
+    # Spatial setup
+    L = 2 # system size in non-dim units
+    sN = 10000 # number of spatial points
+    epsilon = 1e-5 # initial condition noise amplitude
+
+    dx = L / (sN + 1)
+    u0 = clamp.(reduce(hcat, [[10.0, 0.0, 0.0] .+ epsilon .* randn(3) for _ in 1:sN]), 0.0, Inf)
+
+    params = Matrix{Any}(undef, length(logKs), length(ls))
+    retcodes = Matrix{ReturnCode.T}(undef, length(logKs), length(ls))
+    final_states = Matrix{Matrix{Float64}}(undef, length(logKs), length(ls))
+    prog = Progress(length(logKs) * length(ls))
+
+    GC.enable_logging(true)
+    @tasks for i in 1:length(logKs)
+        logK = logKs[i]
+        for (j, l) in enumerate(ls)
+            l = ls[j]
+            mmp = MMParams(;
+                K=10^logK,
+                m=1.0,
+                l=l,
+                k=0.0,
+                c=1.0,
+                d=1.0,
+            )
+            sps = SASMMiCRMParams(
+                mmp_to_mmicrm(mmp),
+                SA[1e-12, 1.0, 1.0],
+                make_cartesianspace_smart(1; dx),
+                # nthreads()
+            )
+            sp = make_smmicrm_problem(sps, copy(u0), 1e8)
+
+            tol = 1000 * eps()
+            s = solve(sp, QNDF();
+                dense=false,
+                save_everystep=false,
+                abstol=tol,
+                reltol=tol,
+                callback=make_timer_callback(60 * 60)
+            )
+
+            params[i, j] = sps
+            retcodes[i, j] = s.retcode
+            final_states[i, j] = s.u[end]
+
+            GC.gc()
+
+            next!(prog)
+            flush(stdout)
+        end
+        # @printf "Finished %d out of %d logK runs\n" i length(logKs)
+    end
+    finish!(prog)
+
+    jldsave("main4_results.jld2"; logKs, ls, params, retcodes, final_states, L, sN, epsilon)
 
     (; params, retcodes, final_states)
 end
