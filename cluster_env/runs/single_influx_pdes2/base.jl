b176345d742d78bd225ef04513151029503b0cd7
Jan Kocka <kockahonza@gmail.com>
2026-04-30 14:40:01 +0100

    progress

--- cluster_env/runs/single_influx_pdes2/base.jl
+++ cluster_env/runs/single_influx_pdes2/base.jl
@@ -56,7 +56,7 @@ function do_pde_runs(systems_fname, out_fname, T, L, sN, sp_epsilon, pde_solve_m
     pde_final_states = Vector{Vector{Matrix{Float64}}}(undef, length(systems_to_run))
     pde_final_Ts = Vector{Vector{Float64}}(undef, length(systems_to_run))
 
-    prog1 = Progress(systems_to_run)
+    prog1 = Progress(length(systems_to_run))
     for groupi in 1:length(systems_to_run)
         systems = systems_to_run[groupi].systems
         num_systems = length(systems)
