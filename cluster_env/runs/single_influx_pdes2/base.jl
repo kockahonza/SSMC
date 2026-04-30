1f2c48d8ba46ae9a4971b5c8d112439fe28165b3
Jan Kocka <kockahonza@gmail.com>
2026-04-30 14:35:11 +0100

    progress

--- cluster_env/runs/single_influx_pdes2/base.jl
+++ cluster_env/runs/single_influx_pdes2/base.jl
@@ -105,7 +105,7 @@ function main1()
     do_pde_runs(
         "systems1.jld2", "data1.jld2",
         1000000000, 5, 5000, 1e-3, # T, L, sN, sp_epsilon
-        10 * 60 * 60, # max time per pde run
+        5 * 60 * 60, # max time per pde run
         8, # number of simultaneous runs
     )
 end
