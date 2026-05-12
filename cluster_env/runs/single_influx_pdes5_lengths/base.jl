3a6ca29f86e0210d6038842cc1ff3128ba398d83
Jan Kocka <kockahonza@gmail.com>
2026-05-12 15:18:48 +0100

    progress

--- cluster_env/runs/single_influx_pdes5_lengths/base.jl
+++ cluster_env/runs/single_influx_pdes5_lengths/base.jl
@@ -17,6 +17,8 @@ function run_1d_pde_sim(ps, u0, T, L, sN;
     solver_threads=nothing,
     tol=100000 * eps(),
     solver=QNDF,
+    save_everystep=false,
+    kwargs...
 )
     dx = L / sN
 
@@ -26,15 +28,16 @@ function run_1d_pde_sim(ps, u0, T, L, sN;
         CartesianSpace{1,Tuple{Periodic}}(SA[dx]),
         solver_threads
     )
-    sp = make_smmicrm_problem(sps, u0, T)
+    sp = make_smmicrm_problem(sps, u0, T; jac_type=:sparse)
 
     s = solve(sp, solver();
         dense=false,
-        save_everystep=false,
         calck=false,
+        save_everystep,
         abstol=tol,
         reltol=tol,
-        callback=make_timer_callback(maxtime)
+        callback=make_timer_callback(maxtime),
+        kwargs...
     )
 
     s
@@ -48,6 +51,7 @@ function run_v2(
     T, L, sN, sp_epsilon;
     ode_u0=[fill(1.0, N); fill(0.0, M)],
     lsks=10 .^ range(-5, 2, 2000),
+    save_pdes_time=false,
     pde_solve_maxtime=2 * 60 * 60,
     run_threads=8,
     solver_threads=div(nthreads(), run_threads),
@@ -90,7 +94,7 @@ function run_v2(
     prog0 = Progress(total_num_runs)
     @tasks for ri in 1:total_num_runs
         # @set ntasks = run_threads
-        p = make_mmicrm_problem(params[ri], copy(ode_u0), T)
+        p = make_mmicrm_problem(params[ri].mmicrm_params, copy(ode_u0), T)
         s = solve(p, QNDF();
             maxiters=100000,
             callback=make_timer_callback(10 * 60),
@@ -99,7 +103,7 @@ function run_v2(
         ode_final_states[ri] = s.u[end]
         ode_final_Ts[ri] = s.t[end]
 
-        lsfunc = linstab_make_k_func(p.p, s.u[end];
+        lsfunc = linstab_make_k_func(params[ri], s.u[end];
             returnobj=:maxeval
         )
         linstab_mrls[ri] = real.(lsfunc.(lsks))
@@ -117,6 +121,7 @@ function run_v2(
     sp_retcodes = Vector{ReturnCode.T}(undef, total_num_runs)
     sp_final_states = Vector{Matrix{Float64}}(undef, total_num_runs)
     sp_final_Ts = Vector{Float64}(undef, total_num_runs)
+    sp_sols = Vector{Any}(undef, total_num_runs)
 
     println("Starting PDE runs near ODE ss")
     flush(stdout)
@@ -131,21 +136,19 @@ function run_v2(
         s = run_1d_pde_sim(params[ri], u0, T, L, sN;
             maxtime=pde_solve_maxtime,
             solver_threads,
+            save_everystep=save_pdes_time,
         )
 
         sp_retcodes[ri] = s.retcode
         sp_final_states[ri] = s.u[end]
         sp_final_Ts[ri] = s.t[end]
-
-        s = nothing
-        GC.gc()
+        sp_sols[ri] = s
 
         next!(prog1)
         flush(stdout)
     end
     finish!(prog1)
     flush(stdout)
-    GC.gc()
 
     # reshape results
     results = (;
@@ -158,6 +161,7 @@ function run_v2(
         sp_retcodes=reshape(sp_retcodes, num_runs, num_Klips),
         sp_final_states=reshape(sp_final_states, num_runs, num_Klips),
         sp_final_Ts=reshape(sp_final_Ts, num_runs, num_Klips),
+        sp_sols=reshape(sp_sols, num_runs, num_Klips),
     )
 
     jldsave(out_fname; results...)
@@ -166,46 +170,30 @@ function run_v2(
 end
 
 """
-Focusing on large enough systems for many peaks.
+A small but high quality test run with li=p=1 and aiming for 10, 15 and 20 peaks.
 """
 function main1()
     Klips_to_run = [
-        # (3.1622776601683795, 0.7284315141008784, 1.0),
-        # (4.393970560760792, 0.9507357626281727, 1.0),
-        # (8.48342898244072, 0.9507357626281727, 1.0),
-        # (19.306977288832506, 0.9507357626281727, 1.0),
-        # (4.393970560760792, 0.999, 1.0),
-        # (61.0540229658533, 0.999, 1.0),
-        # (848.3428982440716, 0.999, 1.0),
-        # (4.393970560760792, 1.0, 1.0),
-        # (16.378937069540637, 1.0, 1.0),
-        # (61.0540229658533, 1.0, 1.0),
-        # (227.58459260747887, 1.0, 1.0),
-        # (848.3428982440716, 1.0, 1.0),
-        (5.0, 1.0, 0.1),
-        (11.0, 1.0, 0.1),
-        (25.0, 1.0, 0.1),
-        (5.0, 0.99, 0.1),
-        (11.0, 0.99, 0.1),
-        (25.0, 0.99, 0.1),
-        (3.0, 0.7, 0.1),
-        (3.0, 0.5, 0.1),
+        (10.782736830354969, 1.0, 1.0),
+        (15.176070792347483, 1.0, 1.0),
+        (19.59437173444063, 1.0, 1.0),
     ]
 
-    num_runs = 8
+    num_runs = 5
     N = 20
 
     DN = 1e-6
 
     T = 1000000000
-    L = 10
-    sN = 2500
+    L = 5
+    sN = 5000
 
     sp_epsilon = 1e-3
 
     run_v2(
-        "data1.jld2",
+        "data1_li1_p1.jld2",
         Klips_to_run, num_runs, N, N, DN,
-        T, L, sN, sp_epsilon
+        T, L, sN, sp_epsilon;
+        save_pdes_time=true,
     )
 end
