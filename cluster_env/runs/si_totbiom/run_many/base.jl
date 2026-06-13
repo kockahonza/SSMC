eda3f4d8084a3d4834a9532fce49a0820b9ef8f7
Jan Kocka <kockahonza@gmail.com>
2026-06-13 15:24:38 +0100

    progress

--- cluster_env/runs/si_totbiom/run_many/base.jl
+++ cluster_env/runs/si_totbiom/run_many/base.jl
@@ -3,7 +3,7 @@ includet("../base.jl")
 
 
 function main1()
-    ps = 10 .^ range(0, -2, 8)
+    ps = 10 .^ range(0, -2, 5)
 
     T = 1e8
     L = 5
@@ -12,52 +12,33 @@ function main1()
 
     save_step = 200
     run_threads = 8
-    
-    f = jldopen("./systems1.jld2")
-    systems = f["systems"]
-    fmd = f["metadata"]
 
-    for (i, (gen_ps, ode_fs)) in enumerate(systems)
-        outfname = 
+    f = jldopen("./systems1_24.jld2")
+    systems = f["systems"]
+
+    println("Starting")
+    flush(stdout)
+    prog = Progress(length(systems))
+    @tasks for (i, (gen_ps, ode_fs)) in enumerate(systems)
+        @set ntasks = run_threads
+
+        outfname = "./data1/r$i.jld2"
         run_1_system_changing_p(
-            "data1_better.jld2", gen_ps, ode_fs, ps,
+            outfname, gen_ps, ode_fs, ps,
             T, L, sN,
             sp_epsilon,
             save_step;
             tol=1e-9,
             maxtime=8 * 60 * 60,
-            run_threads,
+            run_threads=1,
             solver_threads=div(nthreads(), run_threads),
+            solver=TRBDF2,
         )
+
+        GC.gc()
+        next!(prog)
+        flush(stdout)
     end
-
-
-
-
-
-
-    f = jldopen("./sys1.jld2")
-    gen_ps = f["gen_ps"]
-    ode_fs = f["ode_fs"]
-
-    ps = 10 .^ range(0, -2, 8)
-
-    T = 1e8
-    L = 5
-    sN = 1000
-    sp_epsilon = 1e-3
-
-    save_step = 200
-    run_threads=8
-
-    run_1_system_changing_p(
-        "data1_better.jld2", gen_ps, ode_fs, ps,
-        T, L, sN,
-        sp_epsilon,
-        save_step;
-        tol=1e-9,
-        maxtime=6 * 60 * 60,
-        run_threads,
-        solver_threads=div(nthreads(), run_threads),
-    )
+    finish!(prog)
+    flush(stdout)
 end
