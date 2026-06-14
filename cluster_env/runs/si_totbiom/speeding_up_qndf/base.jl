b5a4145f02e1d7f83593c208e7639901a0ae141f
Jan Kocka <kockahonza@gmail.com>
2026-06-14 12:16:32 +0100

    progress

--- cluster_env/runs/si_totbiom/speeding_up_qndf/base.jl
+++ cluster_env/runs/si_totbiom/speeding_up_qndf/base.jl
@@ -8,25 +8,24 @@ function main1()
     ode_fs = f["ode_fs"]
 
     ps = 10 .^ range(0, -2, 8)
-    ps = ps[[1,3]]
 
     T = 1e8
     L = 5
     sN = 1000
     sp_epsilon = 1e-3
 
-    save_step = 200
+    save_step = 10
 
     df = run_1_system_changing_p(
-        "data1_larger_abstol.jld2", gen_ps, ode_fs, ps,
+        "data2.jld2", gen_ps, ode_fs, ps,
         T, L, sN,
         sp_epsilon,
         save_step;
         tol=1e-9,
         abstol=1e-7,
         maxtime=6 * 60 * 60,
-        run_threads=2,
-        solver_threads=16,
+        run_threads=8,
+        solver_threads=1,
         solver=QNDF,
         do_printing=true,
     )
