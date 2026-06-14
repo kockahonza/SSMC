71a80f290aaed747f09344bb83e7ef460b3cd4f4
Jan Kocka <kockahonza@gmail.com>
2026-06-14 11:31:42 +0100

    progress

--- cluster_env/runs/si_totbiom/base.jl
+++ cluster_env/runs/si_totbiom/base.jl
@@ -14,7 +14,7 @@ function change_p(ps, p; DI=1.)
     N, M = get_Ns(ps)
     iri = findfirst(!iszero, ps.K)
 
-    new_ps = copy(ps);
+    new_ps = copy(ps)
     new_ps.Ds[N+1:N+M] .= p * DI
     new_ps.Ds[N+iri] = DI
 
@@ -27,10 +27,13 @@ function run_1_system_changing_p(
     sp_epsilon,
     save_step;
     tol=1e-9,
+    abstol=tol,
+    reltol=tol,
     maxtime=6 * 60 * 60,
     run_threads=8,
     solver_threads=div(nthreads(), run_threads),
     solver=TRBDF2,
+    do_printing=false,
 )
     dx = L / sN
 
@@ -40,14 +43,14 @@ function run_1_system_changing_p(
     # Generate the same u0 for all sims
     pde_u0 = expand_u0_to_size((sN,), ode_fs)
     pde_u0 = perturb_u0_uniform(N, M, pde_u0, sp_epsilon)
-    clamp!(pde_u0, 0., Inf);
+    clamp!(pde_u0, 0., Inf)
 
     metadata = (;
         gen_ps, ode_fs, ps,
         T, L, sN,
         sp_epsilon,
         save_step,
-        tol, maxtime,
+        tol, abstol, reltol, maxtime,
         run_threads, solver_threads,
         dx, N, M, iri, pde_u0,
     )
@@ -58,10 +61,17 @@ function run_1_system_changing_p(
     sol_ts = Vector{Vector{Float64}}(undef, length(ps))
     sol_us = Vector{Vector{Matrix{Float64}}}(undef, length(ps))
     maxresids = Vector{Float64}(undef, length(ps))
+    realtimes = Vector{Float64}(undef, length(ps))
+
+    start_time = time()
 
     @localize pde_u0 @tasks for i in 1:length(ps)
         @set ntasks = run_threads
-        @printf "Starting %d\n" i
+        if do_printing
+            @printf "Starting %d\n" i
+            flush(stdout)
+        end
+
         p = ps[i]
 
         local_ps = change_p(gen_ps, p)
@@ -72,7 +82,7 @@ function run_1_system_changing_p(
             CartesianSpace{1,Tuple{Periodic}}(SA[dx]),
             solver_threads
         )
-        pde_p = make_smmicrm_problem(pde_ps, pde_u0, T; jac_type=:sparse);
+        pde_p = make_smmicrm_problem(pde_ps, pde_u0, T; jac_type=:sparse)
 
         pde_s_t, pde_s_u, scb = make_stepped_saver_callback(pde_u0, save_step)
         push!(pde_s_t, 0.)
@@ -80,10 +90,10 @@ function run_1_system_changing_p(
         pde_s = solve(pde_p, solver();
             dense=false,
             save_everystep=false,
-            abstol=tol,
-            reltol=tol,
+            abstol,
+            reltol,
             callback=CallbackSet(make_timer_callback(maxtime), PositiveDomain(copy(pde_u0); save=false), scb),
-        );
+        )
         push!(pde_s_t, pde_s.t[end])
         push!(pde_s_u, pde_s.u[end])
 
@@ -92,12 +102,19 @@ function run_1_system_changing_p(
         sol_ts[i] = pde_s_t
         sol_us[i] = pde_s_u
         maxresids[i] = smmicrmmaxresid(pde_s)
+        realtimes[i] = time() - start_time
+
+        if do_printing
+            @printf "Finished %d\n" i
+            flush(stdout)
+        end
     end
 
     df = DataFrame(;
-        ps, params, retcodes, sol_ts, sol_us, maxresids
+        ps, params, retcodes, sol_ts, sol_us, maxresids, realtimes
     )
 
     jldsave(outfname; df, metadata)
-end
 
+    df
+end
