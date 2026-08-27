7d4d8b1bb42cbbe9b6b1b000eb83ab3e740f0f3d
Jan Kocka <kockahonza@gmail.com>
2026-08-27 23:32:09 +0100

    progress

---
+++ cluster_env/runs/si_pdes1/main6_commcomp/base.jl
@@ -0,0 +1,15 @@
+################################################################################
+# For Fig.3e, multiple systems each solved at 5 different p
+################################################################################
+include("../base.jl")
+
+function main1()
+    run_pde_setup("systems.jld2", "data";
+        save_step=nothing,
+        abstol=1e-7,
+        reltol=1e-9,
+        maxtime=5 * 60 * 60,
+        run_threads=40,
+        solver_threads=nothing,
+    )
+end
